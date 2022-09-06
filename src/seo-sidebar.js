bcdb09d4a15be812cf557bdc62855fdd962050c8
Matthias Leuffen <matthes@leuffen.de>
2022-09-06 10:45:42 +0200

    update

--- src/seo-sidebar.js
+++ src/seo-sidebar.js
@@ -30,7 +30,7 @@ KaToolsV1.ce_define("seo-sidebar", async function($tpl) {
 
 }
 </style>
-<section class="card position-fixed bottom-0 start-0 end-0 bg-light" style="height: 20px">
+<section class="card position-fixed bottom-0 start-0 end-0 bg-white" style="height: 20px">
 
     <div ka.if="result !== null" class="overflow-scroll p-0  text-nowrap overflow-hidden" style="font-size: 10px">
         <span>Seo-Tool Keywords: [[ $fn.wordCount(document.body.textContent) ]]</span>
